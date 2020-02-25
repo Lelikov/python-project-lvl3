61652828cdece41b873b772559d648081a819361
Alexandr Lelikov <lelikovas@gmail.com>
2020-02-25 13:35:13 +0300

    Added engine

--- page_loader/engine.py
+++ page_loader/engine.py
@@ -1,3 +1,15 @@
+import re
+import os
+import requests
+from urllib.parse import urlparse
+
+REGEX = r'[^a-z0-9]'
+ext = '.html'
+
+
 def loader(url, output):
-    print(url)
-    print(output)
+    parsed_url = urlparse(url)
+    name = re.sub(REGEX, '-', parsed_url.netloc + parsed_url.path)
+    with open(os.path.join(output, name + ext), 'w') as path:
+        path.write(requests.get(url).text)
+    print('Downloading completed')
--- page_loader/script/page_loader.py
+++ page_loader/script/page_loader.py
@@ -5,8 +5,8 @@ from page_loader.engine import loader
 
 def main():
     parser = argparse.ArgumentParser(description='Page loader')
-    parser.add_argument('url')
-    parser.add_argument('-o', '--output', help='set path for output')
+    parser.add_argument('url', type=str)
+    parser.add_argument('-o', '--output', help='set path for output', type=str)
     args = parser.parse_args()
     loader(args.url, args.output)
 
