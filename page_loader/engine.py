f7daacc566da1b2d3daf794c361372cd69fe83d9
Alexandr Lelikov <lelikovas@gmail.com>
2020-02-27 10:46:20 +0300

    Added scheme check

--- page_loader/engine.py
+++ page_loader/engine.py
@@ -4,12 +4,15 @@ import requests
 from urllib.parse import urlparse
 
 REGEX = r'[^a-z0-9]'
-ext = '.html'
+EXT = '.html'
+SCHEME = 'http://'
 
 
 def loader(url, output):
     parsed_url = urlparse(url)
     name = re.sub(REGEX, '-', parsed_url.netloc + parsed_url.path)
-    with open(os.path.join(output, name + ext), 'w') as path:
+    if not parsed_url.scheme:
+        url = SCHEME + url
+    with open(os.path.join(output, name + EXT), 'w') as path:
         path.write(requests.get(url).text)
     print('Downloading completed')
