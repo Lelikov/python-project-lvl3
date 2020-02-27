0d974061e72b3da95cf17046f164dee0e924ad98
Alexandr Lelikov <lelikovas@gmail.com>
2020-02-27 11:12:02 +0300

    Added test

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,2 +1,9 @@
-def test_first():
-    assert 1 == 1
+from page_loader.engine import loader
+import tempfile
+import os
+
+
+def test_name():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        loader('www.lelikova.ru/book', temp_dir)
+        assert os.path.exists('{}/www-lelikova-ru-book.html'.format(temp_dir)) == True
