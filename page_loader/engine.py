b79e88ebfeb50b5731413505ba65d5bc7554d67d
Alexandr Lelikov <lelikovas@gmail.com>
2020-03-04 22:52:25 +0300

    Refactored

--- page_loader/engine.py
+++ page_loader/engine.py
@@ -8,7 +8,7 @@ REGEX = r'[^A-Za-z0-9]'
 EXT = '.html'
 SCHEME = 'http'
 PATH = '_files/'
-TAGS = ['src', 'href']
+ATTRIBUTES = ['src', 'href']
 
 
 def change_url(old_url):
@@ -27,22 +27,23 @@ def url_normalization(path, url):
     return urlunparse((SCHEME, urlparse(url).netloc, urlparse(url).path + '/' + path, '', '', ''))
 
 
-def parser(attr, dom, url, name, output):
-    param = {attr: re.compile(r"")}
-    for tag in dom.find_all(**param):
-        if tag.name == 'a':
-            tag[attr] = url_normalization(tag[attr], url)
-        else:
-            normal_url = url_normalization(tag[attr], url)
-            filename, file_extension = os.path.splitext(normal_url)
-            if filename.startswith('data:'):
-                continue
-            changed_filename = change_url(filename)
-            file = requests.get(normal_url).content
-            with open(os.path.join(output + name + PATH, changed_filename + file_extension),
-                      'wb') as received_file:
-                received_file.write(file)
-            tag[attr] = name + PATH + changed_filename + file_extension
+def parser(dom, url, name, output):
+    for attribute in ATTRIBUTES:
+        param = {attribute: re.compile(r"")}
+        for tag in dom.find_all(**param):
+            if tag.name == 'a':
+                tag[attribute] = url_normalization(tag[attribute], url)
+            else:
+                normal_url = url_normalization(tag[attribute], url)
+                filename, file_extension = os.path.splitext(normal_url)
+                if filename.startswith('data:'):
+                    continue
+                changed_filename = change_url(filename)
+                file = requests.get(normal_url).content
+                with open(os.path.join(output + name + PATH, changed_filename + file_extension),
+                          'wb') as received_file:
+                    received_file.write(file)
+                tag[attribute] = name + PATH + changed_filename + file_extension
 
 
 def loader(url, output):
@@ -57,8 +58,8 @@ def loader(url, output):
         page = BeautifulSoup(requests.get(url).text, "html.parser")
     if not os.path.exists(os.path.join(output, name + PATH)):
         os.makedirs(os.path.join(output, name + PATH))
-    for tag in TAGS:
-        parser(tag, page, url, name, output)
+    for attribute in ATTRIBUTES:
+        parser(page, url, name, output)
     with open(os.path.join(output, name + EXT), 'w') as path:
         path.write(page.prettify())
     print('Downloading completed')
