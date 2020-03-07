f9dc292128ce85d32d3b2ac88b4c04b6aa9a48e7
Alexandr Lelikov <lelikovas@gmail.com>
2020-03-07 21:52:51 +0300

    Added exceptions control

--- page_loader/engine.py
+++ page_loader/engine.py
@@ -4,6 +4,8 @@ from urllib.parse import urlparse, urlunparse
 import requests
 from bs4 import BeautifulSoup
 import logging
+import sys
+from progress.bar import Bar
 
 REGEX = r'[^A-Za-z0-9]'
 EXT = '.html'
@@ -11,40 +13,68 @@ SCHEME = 'http'
 POSTFIX = '_files/'
 ATTRIBUTES = ['src', 'href']
 
-logging.basicConfig(
-    filename='app.log',
-    filemode='w',
-    format='[%(asctime)s] %(module)s - %(levelname)s - %(message)s',
-    level=logging.DEBUG,
-    datefmt='%d-%b-%y %H:%M:%S'
-)
+logging.basicConfig(filemode='w',
+                    datefmt='%d-%b-%y %H:%M:%S',
+                    filename='page_loader.log',
+                    format='[%(asctime)s] %(module)s - %(levelname)s - %(message)s',
+                    )
+logger = logging.getLogger()
+
+console_handler = logging.StreamHandler(sys.stderr)
+console_handler.setLevel(logging.WARNING)
+logger.addHandler(console_handler)
 
 
-def loader(url, output):
+def loader(url, output, log):
+    logger.setLevel(log.upper())
+
+    if not output.endswith('/'):
+        output = os.path.join(output, '')
+        logger.warning('Added / to the end of output')
+
     if url.endswith('/'):
         url = url[:-1]
-        logging.warning('Deleted / in the end')
-    changed_url = change_url(url)
-    try:
-        page = BeautifulSoup(requests.get(url).text, "html.parser")
-    except requests.exceptions.MissingSchema:
+        logger.warning('Deleted last / in the URL')
+
+    if not urlparse(url).scheme:
         url = urlunparse((SCHEME, url, '', '', '', ''))
-        logging.warning('URL was changed. Added http://')
-        page = BeautifulSoup(requests.get(url).text, "html.parser")
+        logger.warning('URL was changed. Added http://')
+
+    changed_url = change_url(url)
+
+    try:
+        get_page = requests.get(url)
+        get_page.raise_for_status()
+        page = BeautifulSoup(get_page.text, "html.parser")
+    except requests.exceptions.RequestException as error:
+        logger.critical(error)
+        sys.exit(11)
+
     if not os.path.exists(os.path.join(output, changed_url + POSTFIX)):
-        os.makedirs(os.path.join(output, changed_url + POSTFIX))
-        logging.warning('Created folder {}'.format(os.path.join(output, changed_url + POSTFIX)))
+
+        try:
+            os.makedirs(os.path.join(output, changed_url + POSTFIX))
+            logger.warning('Created folder {}'.format(os.path.join(output, changed_url + POSTFIX)))
+        except OSError as error:
+            logger.critical(error)
+            sys.exit(21)
+
     parser(page, url, changed_url, output)
-    logging.info('Downloading completed')
-    with open(os.path.join(output, changed_url + EXT), 'w') as path:
-        path.write(page.prettify())
-        logging.info('Modified page created ')
+    logger.info('Downloading completed')
+
+    try:
+        with open(os.path.join(output, changed_url + EXT), 'w') as path:
+            path.write(page.prettify())
+            logger.info('Modified page created ')
+    except OSError as error:
+        logger.critical(error)
+        sys.exit(22)
 
 
 def change_url(old_url):
     parsed_url = urlparse(old_url)
     changed_url = re.sub(REGEX, '-', parsed_url.netloc + parsed_url.path)
-    logging.debug('{} changed to {}'.format(old_url, changed_url))
+    logger.debug('{} changed to {}'.format(old_url, changed_url))
     return changed_url
 
 
@@ -53,12 +83,12 @@ def url_normalization(path, url):
     if parsed_url.scheme:
         return path
     elif parsed_url.netloc:
-        logging.debug('Added {}: to {}'.format(SCHEME, path))
+        logger.debug('Added {}: to {}'.format(SCHEME, path))
         return urlunparse((SCHEME, parsed_url.netloc, parsed_url.path, '', '', ''))
     elif parsed_url.path.startswith('/'):
-        logging.debug('Added {} to {}'.format(url, path))
+        logger.debug('Added {} to {}'.format(url, path))
         return urlunparse((SCHEME, urlparse(url).netloc, parsed_url.path, '', '', ''))
-    logging.debug('Added {}/ to {}'.format(url, path))
+    logger.debug('Added {}/ to {}'.format(url, path))
     return urlunparse((SCHEME, urlparse(url).netloc, urlparse(url).path + '/' + path, '', '', ''))
 
 
@@ -67,26 +97,39 @@ def parser(dom, url, name, output):
         param = {attribute: re.compile(r"")}
         for tag in dom.find_all(**param):
             normalized_url = url_normalization(tag[attribute], url)
-            logging.debug('{} normalized to {}'.format(tag[attribute], normalized_url))
+            logger.debug('{} normalized to {}'.format(tag[attribute], normalized_url))
             if tag.name == 'a':
                 tag[attribute] = normalized_url
             else:
                 filename, file_extension = os.path.splitext(normalized_url)
+
                 if filename.startswith('data:'):
                     continue
                 changed_filename = change_url(filename)
-                logging.debug('New filename for {}{} is {}{}'.format(filename, file_extension,
-                                                                     changed_filename,
-                                                                     file_extension))
-                file = requests.get(normalized_url).content
-                logging.debug('{} is downloaded'.format(normalized_url))
+                logger.debug('New filename for {}{} is {}{}'.format(filename, file_extension,
+                                                                    changed_filename,
+                                                                    file_extension))
+                try:
+                    get_file = requests.get(normalized_url)
+                    get_file.raise_for_status()
+                    file = get_file.content
+                except requests.exceptions.RequestException as error:
+                    logger.critical(error)
+                    sys.exit(12)
+
+                logger.debug('{} is downloaded'.format(normalized_url))
                 file_path = os.path.join(output + name + POSTFIX,
                                          changed_filename + file_extension)
-                with open(file_path, 'wb') as received_file:
-                    received_file.write(file)
-                    logging.debug('{} is created'.format(file_path))
+                try:
+                    with open(file_path, 'wb') as received_file:
+                        received_file.write(file)
+                        logger.debug('{} is created'.format(file_path))
+                except OSError as error:
+                    logger.critical(error)
+                    sys.exit(23)
+
                 tag[attribute] = name + POSTFIX + changed_filename + file_extension
-                logging.debug('New {} is {}'.format(attribute, tag[attribute]))
+                logger.debug('New {} is {}'.format(attribute, tag[attribute]))
 
 
-# loader('hexlet.io', '/Users/alexandrlelikov/Desktop/Python/')
+# loader('httpbin.org/status/404', '/Users/alexandrlelikov/Desktop/Python', 'debug')
--- page_loader/script/page_loader.py
+++ page_loader/script/page_loader.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import os
 
 from page_loader.engine import loader
 
@@ -7,9 +8,13 @@ from page_loader.engine import loader
 def main():
     parser = argparse.ArgumentParser(description='Page loader')
     parser.add_argument('url', type=str)
-    parser.add_argument('-o', '--output', help='set path for output', type=str)
+    parser.add_argument('-o', '--output', help='set path for output', type=str,
+                        default=os.path.join(os.getcwd(), ''))
+    parser.add_argument('-l', '--log', help='set logging verbosity', type=str,
+                        choices=['debug', 'info', 'warning', 'error', 'critical'],
+                        default='warning')
     args = parser.parse_args()
-    loader(args.url, args.output)
+    loader(args.url, args.output, args.log)
 
 
 if __name__ == '__main__':
